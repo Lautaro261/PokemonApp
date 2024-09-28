8ec352be8432f361b85bcf0f4d5c40db7fe6f7eb
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-09-27 22:07:59 -0300

    Integracion de react native paper

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import '../gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
+import { PaperProvider } from 'react-native-paper';
 //import { View, Text, StyleSheet } from 'react-native';
 
 export const App = () => {
   return (
+    <PaperProvider>
     <NavigationContainer>
     <StackNavigator/>
     </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
+      <Button icon="camera" mode="contained" onPress={()=>console.log('Ok')}>
+        Prueba
+      </Button>
     </View>
   );
 };
